a5dc1c2b310f1301908581cbe45a1748be7591b7
Justin <haddenj3@atc.edu>
2017-07-19 08:46:03 -0400

    fingerscrossed

--- awards.php
+++ awards.php
@@ -19,22 +19,13 @@ catch(PDOException $e)
 foreach($OTNeeds as $need)
 {
     include "includes/getSubs.php";
-    if($need['Slot'] == "2017-07-19-1-FPO")
-    {
-        print_r($need['Slot']);
 
-        foreach($OTSubmissions as $submission)
-        {
-            print_r($submission['EmpSubmission']);
-        }
-    }
     $eligible = [];
     $mostEligible = [];
     $found = false;
     foreach($OTSubmissions as $submission)
     {
-    	//print_r($submission);
-    	//print_r($need);
+    	print_r("-|-".$need['Slot']."---".$submission['EmpSubmission']);
         if($submission['EmpSubmission'] == $need['Slot'])
         {
             array_push($eligible, $submission);
