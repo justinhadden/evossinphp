369ac7dbfb8832e06339eba280c52f193f22a9e2
Justin <haddenj3@atc.edu>
2017-07-19 09:09:30 -0400

    Gotit!

--- includes/getSubs.php
+++ includes/getSubs.php
@@ -5,7 +5,7 @@ include "includes/dbconnection.php";
 try
 {
     $sql = "SELECT concat(SubmissionDate,'-',Shift,'-',submission.JobCode) AS EmpSubmission,
-        SubmissionDate,EmpComment,employee.EmpID,OTHours,Shift,submission.JobCode
+        SubmissionDate,EmpComment,employee.EmpID,OTHours,Shift,submission.JobCode,OTBlock
         FROM employee,submission
         WHERE employee.EmpID=submission.EmpID
         AND SubmissionDate <= CURDATE() + 2";
