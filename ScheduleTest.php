214e08e9f42ce137ef05375d80a70276bb65952a
Justin <haddenj3@atc.edu>
2017-07-20 09:45:06 -0400

    SchedScript

--- ScheduleTest.php
+++ ScheduleTest.php
@@ -12,20 +12,125 @@ $calDiff = $calDate->diff($baseDate)->format("%a");
 echo "Number of days since 2016-01-20: ".$calDiff."\n";
 echo "Shift to evaluate: ".$shiftNum."\n";
 
-$daysIntoRot = ($calDiff % 28) / 7;
+$daysIntoRot = ($calDiff % 28);
 
-if(floor($daysIntoRot) == 0)
+$shiftCode = $daysIntoRot / 7;
+
+if(floor($shiftCode) == 0)
 {
-    echo "This is A Shift\n";
+    if($shiftNum == "1")
+    {
+        echo "This is A Shift\n";
+    }
+    elseif($shiftNum == "2")
+    {
+        if($daysIntoRot == 0)
+        {
+            echo "This is C Shift\n";
+        }
+        else
+        {
+            echo "This is B Shift\n";
+        }
+    }
+    else
+    {
+        if($daysIntoRot < 2)
+        {
+            echo "This is D Shift\n";
+        }
+        else
+        {
+            echo "This is C Shift\n";
+        }
+    }
 }
-elseif(floor($daysIntoRot) == 1)
+elseif(floor($shiftCode) == 1)
 {
-    echo "This is D shift\n";
+     if($shiftNum == "1")
+    {
+        echo "This is D Shift\n";
+    }
+    elseif($shiftNum == "2")
+    {
+        if($daysIntoRot == 0)
+        {
+            echo "This is B Shift\n";
+        }
+        else
+        {
+            echo "This is A Shift\n";
+        }
+    }
+    else
+    {
+        if($daysIntoRot < 2)
+        {
+            echo "This is C Shift\n";
+        }
+        else
+        {
+            echo "This is B Shift\n";
+        }
+    }
 }
-elseif(floor($daysIntoRot) == 2)
+elseif(floor($shiftCode) == 2)
 {
-    echo "This is C shift\n";
+     if($shiftNum == "1")
+    {
+        echo "This is C Shift\n";
+    }
+    elseif($shiftNum == "2")
+    {
+        if($daysIntoRot == 0)
+        {
+            echo "This is A Shift\n";
+        }
+        else
+        {
+            echo "This is D Shift\n";
+        }
+    }
+    else
+    {
+        if($daysIntoRot < 2)
+        {
+            echo "This is B Shift\n";
+        }
+        else
+        {
+            echo "This is A Shift\n";
+        }
+    }
 }
-else {
-    echo "This is B Shift\n";
+else
+{
+    if($shiftNum == "1")
+    {
+        echo "This is B Shift\n";
+    }
+    elseif($shiftNum == "2")
+    {
+        if($daysIntoRot == 0)
+        {
+            echo "This is D Shift\n";
+        }
+        else
+        {
+            echo "This is C Shift\n";
+        }
+    }
+    else
+    {
+        if($daysIntoRot < 2)
+        {
+            echo "This is A Shift\n";
+        }
+        else
+        {
+            echo "This is D Shift\n";
+        }
+    }
 }
+
+?>
