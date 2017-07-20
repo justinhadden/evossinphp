3f48bae8acd9032bf5ee4ddea2b13aa2a8283e09
Justin <haddenj3@atc.edu>
2017-07-20 09:27:56 -0400

    SchedScript

--- ScheduleTest.php
+++ ScheduleTest.php
@@ -5,7 +5,7 @@ $calDate = new DateTime($argv[1]);
 
 $shiftNum = $argv[2];
 
-$baseDate = new DateTime("2017-01-18");
+$baseDate = new DateTime("2016-01-20");
 
 $calDiff = $calDate->diff($baseDate)->format("%a");
 
