b430a1088556b91924f4b8c365dc6ee7154d78ee
Justin <haddenj3@atc.edu>
2017-07-20 08:52:30 -0400

    SchedScript

--- ScheduleTest.php
+++ ScheduleTest.php
@@ -7,6 +7,11 @@ $calDate = date('Y-m-d',$tempdate);
 
 $shiftNum = $argv[2];
 
+$baseDate = date('Y-m-d', "2017-01-18");
 
-echo "This is the date - ".$calDate."\n";
+$datediff = $baseDate - $calDate;
+
+$calDiff = floor($datediff / (60 * 60 * 24));
+
+echo "This is the date - ".$calDiff."\n";
 echo "This is the shift - ".$shiftNum."\n";
