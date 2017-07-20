cb368c6fa2f6c5f83dd1b9decc2eb8bbab86a2ba
Justin <haddenj3@atc.edu>
2017-07-20 09:17:18 -0400

    SchedScript

--- ScheduleTest.php
+++ ScheduleTest.php
@@ -9,5 +9,23 @@ $baseDate = new DateTime("2017-01-18");
 
 $calDiff = $calDate->diff($baseDate)->format("%a");
 
-echo "This is the date - ".$calDiff."\n";
-echo "This is the shift - ".$shiftNum."\n";
+echo "Number of days since 2017-01-18: ".$calDiff."\n";
+echo "Shift to evaluate: ".$shiftNum."\n";
+
+$daysIntoRot = $calDiff % 28 + 1;
+
+if(floor($daysIntoRot) == 0)
+{
+    echo "This is A Shift".
+}
+elseif(floor($daysIntoRot) == 1)
+{
+    echo "This is D shift";
+}
+elseif(floor($daysIntoRot) == 2)
+{
+    echo "This is C shift";
+}
+else {
+    echo "This is B Shift";
+}
