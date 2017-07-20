4b1ac22032f92611748aa7c9c7cf56689f32403c
Justin <haddenj3@atc.edu>
2017-07-20 08:45:23 -0400

    SchedScript

--- ScheduleTest.php
+++ ScheduleTest.php
@@ -1,7 +1,10 @@
 <?php
 
 
-foreach($argv as $value)
-{
-    echo "$value\n";
-}
+$calDate = strtotime($argv[0]);
+
+$shiftNum = $argv[1];
+
+
+echo "This is the date - ".$calDate."\n";
+echo "This is the shift - ".$shiftNum."\n";
