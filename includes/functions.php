9f26c403c0c30b04c30a1b97c68ad67cb00f8e4f
Justin <haddenj3@atc.edu>
2017-08-03 10:25:05 -0400

    AGOEJDJA

--- includes/functions.php
+++ includes/functions.php
@@ -185,8 +185,7 @@ function getApplicableSubmissions($needDate,$needShift,$needJobCode,$offGoingShi
 			and submission.JobCode = '$needJobCode'
 			and awarded = 0;
 			and ShiftCode != '$shiftCode'
-            ORDER BY CASE 	WHEN ShiftCode = '$offGoingShiftCode' THEN '1' 
-							ELSE OTHoursWorked+OPOTHours,DeptSeniority,ShiftCode END");
+            ORDER BY CASE WHEN ShiftCode = '$offGoingShiftCode' THEN 1 ELSE 2, OTHoursWorked+OPOTHours,DeptSeniority");
         $results = $statement->fetchAll(PDO::FETCH_ASSOC);
     }
     catch(PDOException $e)
